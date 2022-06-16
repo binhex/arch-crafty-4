dc588b6fd4d9de868e694b27675c4ded95d50c2d
binhex <megalith01@gmail.com>
2022-06-16 11:53:17 +0100

    fix up start script

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -2,7 +2,7 @@
 
 crafty_install_path="/opt/crafty"
 
-mkdir -p '/config/crafty/db' '/config/crafty/config' '/config/crafty/servers'
+mkdir -p '/config/crafty/app/config' '/config/crafty/logs' '/config/crafty/import' '/config/crafty/servers' '/config/crafty/backups'
 
 config_filepath='/config/crafty/config/docker_config.yml'
 
@@ -11,17 +11,17 @@ if [ ! -f "${config_filepath}" ]; then
 	cp "${crafty_install_path}/configs/docker_config.yml" "${config_filepath}"
 fi
 
-# modify config file
-###
+# symlink app to config
+source '/usr/local/bin/utils.sh' && symlink --src-path "${crafty_install_path}/app/config" --dst-path '/config/crafty/app/config' --link-type 'softlink' --log-level 'WARN'
 
-# tells crafty if it should activate the console and not loop forever
-#sed -i -E "s~daemon_mode: true~daemon_mode: false~g" "${config_filepath}"
+# symlink logs to config
+source '/usr/local/bin/utils.sh' && symlink --src-path "${crafty_install_path}/logs" --dst-path '/config/crafty/logs' --link-type 'softlink' --log-level 'WARN'
 
-# tells crafty where to store the database it creates
-sed -i -E 's~db_dir: "/crafty_db"~db_dir: "/config/crafty/db"~g' "${config_filepath}"
+# symlink import to config
+source '/usr/local/bin/utils.sh' && symlink --src-path "${crafty_install_path}/import" --dst-path '/config/crafty/import' --link-type 'softlink' --log-level 'WARN'
 
-# symlink certs to config
-source '/usr/local/bin/utils.sh' && symlink --src-path "${crafty_install_path}/app/web/certs" --dst-path '/config/crafty/certs' --link-type 'softlink' --log-level 'WARN'
+# symlink servers to config
+source '/usr/local/bin/utils.sh' && symlink --src-path "${crafty_install_path}/servers" --dst-path '/config/crafty/servers' --link-type 'softlink' --log-level 'WARN'
 
 # symlink backups to config
 source '/usr/local/bin/utils.sh' && symlink --src-path "${crafty_install_path}/backups" --dst-path '/config/crafty/backups' --link-type 'softlink' --log-level 'WARN'
@@ -33,4 +33,4 @@ cd "${crafty_install_path}"
 source ./env/bin/activate
 
 # run app
-python3 "${crafty_install_path}/crafty.py" --config "${config_filepath}"
+python3 "${crafty_install_path}/main.py"
