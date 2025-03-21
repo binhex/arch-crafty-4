8e3f710910fbd09550a90b9baaeb7cc9b29855c9
binhex <megalith01@gmail.com>
2025-03-21 18:18:38 +0000

    fix incorrect push

--- run/nobody/start.sh
+++ run/nobody/start.sh
@@ -1,17 +1,43 @@
 #!/usr/bin/dumb-init /bin/bash
 
+set -x
 # set install location for crafty
-siphonator_install_path="/opt/siphonator"
+crafty_install_path="/opt/crafty"
 
-# set path to store configuration files
-config_path="/config/siphonator"
+# crafty lock file (indicates crafty process running) path, deleted on startup (process not running)
+crafty_session_lock_filepath="/config/crafty/app/config/session.lock"
+
+# symlink app to config
+source '/usr/local/bin/utils.sh' && symlink --src-path "${crafty_install_path}/app/config" --dst-path '/config/crafty/app/config' --link-type 'softlink'
+
+# symlink logs to config
+source '/usr/local/bin/utils.sh' && symlink --src-path "${crafty_install_path}/logs" --dst-path '/config/crafty/logs' --link-type 'softlink'
+
+# symlink import to config
+source '/usr/local/bin/utils.sh' && symlink --src-path "${crafty_install_path}/import" --dst-path '/config/crafty/import' --link-type 'softlink'
+
+# symlink servers to config
+source '/usr/local/bin/utils.sh' && symlink --src-path "${crafty_install_path}/servers" --dst-path '/config/crafty/servers' --link-type 'softlink'
+
+# symlink backups to config
+source '/usr/local/bin/utils.sh' && symlink --src-path "${crafty_install_path}/backups" --dst-path '/config/crafty/backups' --link-type 'softlink'
+
+cd "${crafty_install_path}"
+
+# if any default config files are missing (no clobber) then copy from 'config-backup' folder (created by utils.sh symlink)
+cp -n -R "${crafty_install_path}/app/config-backup/"* "${crafty_install_path}/app/config/"
 
 # activate virtualenv where requirements have been installed from install.sh
-source "${siphonator_install_path}/venv/bin/activate"
+source "${crafty_install_path}/venv/bin/activate"
+
+# remove previous session lock file if it exists (contains pid and datetime)
+if [[ -f "${crafty_session_lock_filepath}" ]]; then
+	echo "Removing previous session.lock file '${crafty_session_lock_filepath}'..."
+	rm -f "${crafty_session_lock_filepath}"
+fi
+
+# overwrite file containing latest version info
+cp "${crafty_install_path}/app/config-backup/version.json" '/config/crafty/app/config/version.json'
 
 # run app
-python3 "${siphonator_install_path}/siphonator.py" \
-  --log-path "${config_path}/logs" \
-  --pid-path "${config_path}/pids" \
-  --config-path "${config_path}/configs" \
-  --db-path "${config_path}/db"
+python3 "${crafty_install_path}/main.py"
